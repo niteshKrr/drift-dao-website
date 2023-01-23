2a63721492290a725f680eb0233e9410b2469d4f
deependujha <deependujha21@gmail.com>
2023-01-23 18:19:13 +0530

    team image updated and errors of contact form resolved

--- components/main/CardComponent.tsx
+++ components/main/CardComponent.tsx
@@ -1,45 +1,49 @@
-import React from "react";
-import { Card, Grid, Row, Text } from "@nextui-org/react";
-import Image from "next/image";
+import React from 'react';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+import Image from 'next/image';
 
 type CardComponentProp = {
-  name: string;
-  email: string;
-  twitter: string;
+	name: string;
+	email: string;
+	twitter: string;
 };
 
-const CardComponent = ({name,email,twitter}: CardComponentProp) => {
-  return (
-    <div style={{ width: "350px", height: "350px" }}>
-      <Card isPressable>
-        <Card.Body>
-          <Image
-            src={"/images/team/nitesh.jpeg"}
-            width="350"
-            height="350"
-            alt="image"
-          />
-        </Card.Body>
-        <Card.Footer css={{ justifyItems: "flex-start" }}>
-          <Row wrap="wrap" justify="space-between" align="center">
-            <div className="font-bold text-2xl tracking-tight text-purple-900">
-              {name}
-            </div>
-            <a href={`mailto: ${email}`}>
-              <div className="font-normal text-pink-500 my-2">
-                email: {email}
-              </div>
-            </a>
-            <a href={`https://twitter.com/${twitter}`} target="_blank">
-              <div className="font-normal text-purple-900 my-2">
-                twitter: {twitter}
-              </div>
-            </a>
-          </Row>
-        </Card.Footer>
-      </Card>
-    </div>
-  );
+const CardComponent = ({ name, email, twitter }: CardComponentProp) => {
+	return (
+		<div style={{ width: '350px', height: '350px' }}>
+			<Card isPressable>
+				<Card.Body>
+					<Image
+						src={`/images/team/${twitter}.jpeg`}
+						width="350"
+						height="350"
+						alt="image"
+					/>
+				</Card.Body>
+				<Card.Footer css={{ justifyItems: 'flex-start' }}>
+					<Row wrap="wrap" justify="space-between" align="center">
+						<div className="font-bold text-2xl tracking-tight text-purple-900">
+							{name}
+						</div>
+						<a href={`mailto: ${email}`}>
+							<div className="font-normal text-pink-500 my-1">
+								email: {email}
+							</div>
+						</a>
+						<a
+							href={`https://twitter.com/${twitter}`}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<div className="font-normal text-purple-900 my-1">
+								twitter: {twitter}
+							</div>
+						</a>
+					</Row>
+				</Card.Footer>
+			</Card>
+		</div>
+	);
 };
 
 export default CardComponent;
--- components/main/ContactForm.tsx
+++ components/main/ContactForm.tsx
@@ -1,150 +1,146 @@
-import React from "react";
-import { useState } from "react";
-import axios from "axios";
-import swal from "sweetalert";
-import { Input, Textarea, Button } from "@nextui-org/react";
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import swal from 'sweetalert';
+import { Input, Textarea, Button, FormElement } from '@nextui-org/react';
 
 const initialValues = {
-  name: "",
-  email: "",
-  message: "",
+	name: '',
+	email: '',
+	message: '',
 };
 
 const ContactForm = () => {
-  const [formdata, setformdata] = useState(initialValues);
+	const [formdata, setformdata] = useState(initialValues);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setformdata({
-      ...formdata,
-      [name]: value,
-    });
-  };
+	const handleInputChange = (event: React.ChangeEvent<FormElement>) => {
+		const { name, value } = event.target;
+		setformdata({
+			...formdata,
+			[name]: value,
+		});
+	};
 
-  // Validate if a string an email
-  function validateEmail(email: string) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  }
+	// Validate if a string an email
+	function validateEmail(email: string) {
+		var re = /\S+@\S+\.\S+/;
+		return re.test(email);
+	}
 
-  const formSubmit = async (event: React.ChangeEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    const { name, email, message } = formdata;
-    let msg = "";
+	const formSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		// event.preventDefault();
+		const { name, email, message } = formdata;
+		let msg = '';
 
-    if (name === "") {
-      msg = "Please enter your name.";
-    } else if (email === "") {
-      msg = "Please enter your email.";
-    } else if (message === "") {
-      msg = "Please enter your message.";
-    } else if (validateEmail(email) === false) {
-      msg = "Please enter a valid email address";
-    }
+		if (name === '') {
+			msg = 'Please enter your name.';
+		} else if (email === '') {
+			msg = 'Please enter your email.';
+		} else if (message === '') {
+			msg = 'Please enter your message.';
+		} else if (validateEmail(email) === false) {
+			msg = 'Please enter a valid email address';
+		}
 
-    if (msg) {
-      swal({
-        title: "Error!",
-        text: msg,
-        icon: "error",
-        buttons: {
-          cancel: {
-            text: "Close",
-            value: true,
-            visible: true,
-            className: "",
-            closeModal: true,
-          },
-        },
-      });
-    } else {
-      swal({
-        title: "Thank you!",
-        text: "Thank you, we will soon reach out to you.",
-        icon: "success",
-      });
-      // sendMailToUs(name, email, message);
-      setformdata(initialValues);
-      const result = await axios({
-        method: "post",
-        url: "https://www.backend.drift-dao.com/contactForm",
-        headers: {
-          authorized_access_token:
-            process.env.NEXT_PUBLIC_AUTHORIZED_ACCESS_TOKEN,
-        },
-        data: {
-          name,
-          email,
-          message,
-        },
-      });
-      // console.log(result);
-    }
-  };
+		if (msg) {
+			swal({
+				title: 'Error!',
+				text: msg,
+				icon: 'error',
+				buttons: {
+					cancel: {
+						text: 'Close',
+						value: true,
+						visible: true,
+						className: '',
+						closeModal: true,
+					},
+				},
+			});
+		} else {
+			swal({
+				title: 'Thank you!',
+				text: 'Thank you, we will soon reach out to you.',
+				icon: 'success',
+			});
+			// sendMailToUs(name, email, message);
+			setformdata(initialValues);
+			const result = await axios({
+				method: 'post',
+				url: 'https://www.backend.drift-dao.com/contactForm',
+				data: {
+					name,
+					email,
+					message,
+				},
+			});
+			// console.log(result);
+		}
+	};
 
-  return (
-    <div className="max-w-lg">
-      {/* <form className="flex flex-col gap-4"> */}
-        <div>
-          <div className="mb-2 block">
-            <Input
-              style={{ color: "white" }}
-              width="80%"
-              label="Your name"
-              id="name11"
-              type="text"
-              value={formdata.name}
-              onChange={handleInputChange}
-              name="name"
-              required={true}
-            />
-          </div>
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Input
-              width="80%"
-              label="Your email"
-              id="email1"
-              type="email"
-              value={formdata.email}
-              onChange={handleInputChange}
-              name="email"
-              placeholder="@drift-dao.com"
-              required={true}
-            />
-          </div>
-        </div>
+	return (
+		<div className="max-w-2xl">
+			<div className="flex flex-col gap-4">
+				<div>
+					<div className="mb-2 block">
+						<label htmlFor="name" className="text-lg">
+							Your name
+						</label>
+						<br />
+						<Input
+							width="80%"
+							id="name"
+							placeholder="Vitalik Buterin"
+							type="text"
+							value={formdata.name}
+							onChange={handleInputChange}
+							name="name"
+							aria-labelledby="name"
+						/>
+					</div>
+				</div>
+				<div>
+					<div className="mb-2 block">
+						<label htmlFor="email" className="text-lg">
+							Your email
+						</label>
+						<br />
+						<Input
+							id="email"
+							width="80%"
+							value={formdata.email}
+							onChange={handleInputChange}
+							name="email"
+							placeholder="drift.dao@gmail.com"
+							aria-labelledby="alpha"
+						/>
+					</div>
+				</div>
 
-        <div id="textarea">
-          <div className="mb-2 block">
-            <Textarea
-              width="80%"
-              label="Your message"
-              id="comment"
-              value={formdata.message}
-              onChange={handleInputChange}
-              name="message"
-              placeholder="Leave a comment..."
-              required={true}
-              rows={4}
-            />
-          </div>
-        </div>
-        <div>
-          <Button
-            shadow
-            color="success"
-            onClick={formSubmit}
-            type="submit"
-            auto
-          >
-            Submit
-          </Button>
-        </div>
-      {/* </form> */}
-    </div>
-  );
+				<div className="mb-2 block">
+					<label htmlFor="message" className="text-lg ">
+						Message
+					</label>
+					<br />
+					<Textarea
+						id="message"
+						width="80%"
+						aria-labelledby="message"
+						value={formdata.message}
+						onChange={handleInputChange}
+						name="message"
+						placeholder="Leave a message..."
+						rows={4}
+					/>
+				</div>
+				<div>
+					<Button shadow color="success" onClick={formSubmit} auto>
+						Submit
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ContactForm;
