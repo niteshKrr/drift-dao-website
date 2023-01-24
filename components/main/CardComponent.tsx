b9e0f69f46df6783b69d0c631e6beaac3fa038c7
Niteshkumar <nkumar35101@gmail.com>
2023-01-24 14:45:43 +0530

    team section done

--- components/main/CardComponent.tsx
+++ components/main/CardComponent.tsx
@@ -10,40 +10,51 @@ type CardComponentProp = {
 
 const CardComponent = ({ name, email, twitter }: CardComponentProp) => {
 	return (
-		<div className="max-w-sm m-auto py-6"  style={{ maxWidth: '350px', maxHeight: '350px' }}>
-			<Card isPressable>
-				<Card.Body>
-					<Image
-						src={`/images/team/${twitter}.jpeg`}
-						width="350"
-						height="350"
-						alt="image"
-					/>
-				</Card.Body>
-				<Card.Footer css={{ justifyItems: 'flex-start' }}>
-					<Row wrap="wrap" justify="space-between" align="center">
-						<div className="font-bold text-2xl tracking-tight text-purple-900">
-							{name}
-						</div>
-						<a href={`mailto: ${email}`}>
-							<div className="font-normal text-pink-500 my-1">
-								email: {email}
-							</div>
-						</a>
-						<a
-							href={`https://twitter.com/${twitter}`}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div className="font-normal text-purple-900 my-1">
-								twitter: {twitter}
-							</div>
-						</a>
-					</Row>
-				</Card.Footer>
-			</Card>
-		</div>
-	);
+    <div
+      className={`max-w-sm m-auto py-6`}
+      style={{ maxWidth: "350px", maxHeight: "350px" ,  }}
+    >
+      {/* <style jsx>{`
+        @media (max-width: 600px) {
+          div {
+            width: 250px;
+            margintop: 50px;
+          }
+        }
+      `}</style> */}
+      <Card isPressable>
+        <Card.Body>
+          <Image
+            src={`/images/team/${twitter}.jpeg`}
+            width="350"
+            height="350"
+            alt="image"
+          />
+        </Card.Body>
+        <Card.Footer css={{ justifyItems: "flex-start" }}>
+          <Row wrap="wrap" justify="space-between" align="center">
+            <div className="font-bold text-2xl tracking-tight text-purple-900">
+              {name}
+            </div>
+            <a href={`mailto: ${email}`}>
+              <div className="font-normal text-pink-500 my-1">
+                email: {email}
+              </div>
+            </a>
+            <a
+              href={`https://twitter.com/${twitter}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="font-normal text-purple-900 my-1">
+                twitter: {twitter}
+              </div>
+            </a>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 };
 
 export default CardComponent;
--- components/main/TeamComponent.tsx
+++ components/main/TeamComponent.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import CardComponent from "./CardComponent";
+import styles from "@/styles/CardComponent.module.css";
+
 
 const TeamComponent = () => {
   return (
     <div className="bg-[url('/images/bg/bg1.jpg')]" id="team">
       <div className="text-white text-6xl text-center pt-20">Our team</div>
-      <div className="sm:flex items-center justify-around sm:h-screen py-10">
-        <div>
+      <div className="sm:flex items-center justify-around sm:h-screen py-8">
+        <div className={`${styles.phone} m-auto`}>
           <CardComponent
             name="Nitesh Kumar"
             email="nkumar35101@gmail.com"
             twitter="nkumar35101"
           />
         </div>
-        <div>
+        <div className={`${styles.phone} m-auto`}>
           <CardComponent
             name="Deependu Jha"
             email="deependujha21@gmail.com"
